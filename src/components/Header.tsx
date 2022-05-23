09a5839fdfb768f638e206586b39ce6ec9a8e97a
joyjsmun <joy.js.mun@gmail.com>
2022-05-23 20:32:57 +0900

    ADD: animation for searchbar, nav menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,17 @@
 
-import { motion } from "framer-motion"
+import { motion, useAnimation, useViewportScroll } from "framer-motion"
+import { useEffect, useState } from "react"
 import { Link, useLocation } from "react-router-dom"
 import styled from "styled-components"
 
-const Nav = styled.nav`
+const Nav = styled(motion.nav)`
     width: 100vw;
     height: 100px;
+    position: fixed;
     display: flex;
     background-color: red;
     padding: 60px;
+    color: white;
     justify-content: space-between;
 `
 const Menu = styled.ul`
@@ -48,8 +51,28 @@ const Circle = styled(motion.div)`
 `
 
 
-const Search = styled.div``
-const SearchInput = styled.input``
+const Search = styled(motion.div)`
+    display: flex;
+    justify-content: center;
+    width: 200px;
+    height: 30px;
+`
+
+const SearchIcon = styled(motion.svg)`
+    margin-right: 10px;
+`
+const SearchInput = styled(motion.input)`
+    border:none;
+    transform-origin: right center;
+    width: 150px;
+    height: 25px;
+    background-color: transparent;
+    color:white;
+    ::placeholder {color: white;}
+    :focus {
+     border: none
+}
+`
 
 const icon = {
     hidden: {
@@ -62,14 +85,49 @@ const icon = {
     }
   }
 
+const navVariants = {
+    top:{
+        backgroundColor : "rgba(0,0,0,0)"
+    },
+    scroll:{
+        backgroundColor: "rgba(0,0,0,1)"
+    }
+
+}
 
 function Header(){
-
+    const [searchOpen,setSearchOpen] = useState(false)
     const location = useLocation()
+    const inputAnimation = useAnimation();
+    const {scrollY} = useViewportScroll();
+    const navAnimation = useAnimation()
+
+    useEffect(()=>{
+        scrollY.onChange(()=>{
+            if(scrollY.get() > 80){
+                navAnimation.start("top")
+            }else{
+                navAnimation.start("scroll")
+            }
+        })
+    },[scrollY,navAnimation])
+    
+    
+  
+    const toggleSearch = () => {
+        if (searchOpen) {
+          inputAnimation.start({
+            scaleX: 0,
+          });
+        } else {
+          inputAnimation.start({ scaleX: 1 });
+        }
+        setSearchOpen((prev) => !prev);
+      };
     
     return (
         <>
-        <Nav>
+        <Nav variants={navVariants} animate={navAnimation} initial={"top"}>
             <Menu>
             <Logo>
                 <motion.svg
@@ -92,18 +150,23 @@ function Header(){
                 <Item>
                     <Link to="/">
                     Home
-                    {location.pathname === "/" ? <Circle layoutId="circle" /> : null}
+                    {location.pathname === "/" ? <Circle layoutId="dot" /> : null}
                 </Link>
                 </Item >   
                 <Item>
                 <Link to="/tv">
                     TV Show
-                    {location.pathname === "/tv" ? <Circle layoutId="circle" /> : null}
+                    {location.pathname === "/tv" ? <Circle layoutId="dot" /> : null}
                 </Link>
                 </Item>
             </Menu>
-            <Search>
-                <SearchInput />
+            <Search animate={{border: searchOpen ? "1px solid": 0}} transition={{type:"linear"}}>
+            <SearchIcon
+             onClick={toggleSearch}
+            animate={{ x: searchOpen ? 0 : 185}}
+            transition={{ type: "linear" }}
+            width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M13 11C13 13.7614 10.7614 16 8 16C5.23858 16 3 13.7614 3 11C3 8.23858 5.23858 6 8 6C10.7614 6 13 8.23858 13 11ZM14.0425 16.2431C12.5758 17.932 10.4126 19 8 19C3.58172 19 0 15.4183 0 11C0 6.58172 3.58172 3 8 3C12.4183 3 16 6.58172 16 11C16 11.9287 15.8417 12.8205 15.5507 13.6497L24.2533 18.7028L22.7468 21.2972L14.0425 16.2431Z" fill="currentColor"></path></SearchIcon>
+                <SearchInput  animate={inputAnimation}  initial={{ scaleX: 0 }} transition={{ type: "linear" }}  placeholder="Search Movie or Show"/>
             </Search>
         </Nav>
         </>
