d7735e367e3c7b5d3b31363f89b88134058aac94
joyjsmun <joy.js.mun@gmail.com>
2022-05-26 02:43:36 +0900

    ADD: send keyword to url

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
 
 import { motion, useAnimation, useViewportScroll } from "framer-motion"
 import { useEffect, useState } from "react"
-import { Link, useLocation } from "react-router-dom"
+import { useForm } from "react-hook-form"
+import { Link, useLocation, useNavigate } from "react-router-dom"
 import styled from "styled-components"
 
 const Nav = styled(motion.nav)`
@@ -51,7 +52,7 @@ const Circle = styled(motion.div)`
 `
 
 
-const Search = styled(motion.div)`
+const Search = styled(motion.form)`
     display: flex;
     justify-content: center;
     width: 200px;
@@ -95,12 +96,21 @@ const navVariants = {
 
 }
 
+interface IForm{
+    keyword:string
+}
+
 function Header(){
     const [searchOpen,setSearchOpen] = useState(false)
     const location = useLocation()
+    const navigate = useNavigate()
     const inputAnimation = useAnimation();
     const {scrollY} = useViewportScroll();
     const navAnimation = useAnimation()
+    const {register,handleSubmit} = useForm<IForm>()
+    const onValid = (data:IForm) => {
+        navigate(`/search?keyword=${data.keyword}`);
+    }
 
     useEffect(()=>{
         scrollY.onChange(()=>{
@@ -160,13 +170,15 @@ function Header(){
                 </Link>
                 </Item>
             </Menu>
-            <Search animate={{border: searchOpen ? "1px solid": 0}} transition={{type:"linear"}}>
+            <Search onSubmit={handleSubmit(onValid)} animate={{border: searchOpen ? "1px solid": 0}} transition={{type:"linear"}}>
             <SearchIcon
              onClick={toggleSearch}
             animate={{ x: searchOpen ? 0 : 185}}
             transition={{ type: "linear" }}
             width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M13 11C13 13.7614 10.7614 16 8 16C5.23858 16 3 13.7614 3 11C3 8.23858 5.23858 6 8 6C10.7614 6 13 8.23858 13 11ZM14.0425 16.2431C12.5758 17.932 10.4126 19 8 19C3.58172 19 0 15.4183 0 11C0 6.58172 3.58172 3 8 3C12.4183 3 16 6.58172 16 11C16 11.9287 15.8417 12.8205 15.5507 13.6497L24.2533 18.7028L22.7468 21.2972L14.0425 16.2431Z" fill="currentColor"></path></SearchIcon>
-                <SearchInput  animate={inputAnimation}  initial={{ scaleX: 0 }} transition={{ type: "linear" }}  placeholder="Search Movie or Show"/>
+                <SearchInput  
+                {...register("keyword",{required:true,minLength:2})}
+                animate={inputAnimation}  initial={{ scaleX: 0 }} transition={{ type: "linear" }}  placeholder="Search Movie or Show"/>
             </Search>
         </Nav>
         </>
