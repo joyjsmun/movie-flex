00528af3fc5a0fa4b31a7313170157b8506965ec
joyjsmun <joy.js.mun@gmail.com>
2022-05-26 02:52:50 +0900

    ADD: useSearchParams()

--- src/routes/Search.tsx
+++ src/routes/Search.tsx
@@ -1,4 +1,10 @@
+import { useLocation, useSearchParams } from "react-router-dom";
+
 function Search(){
+    const [searchParams] = useSearchParams();
+    const keyword = searchParams.get("keyword");
+    console.log(keyword)
+    
     return <div>Search</div>
 }
 
