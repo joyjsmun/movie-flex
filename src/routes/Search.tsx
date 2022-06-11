727e9eb2ee3de9b6e07c69d2a3304d88876c179d
joyjsmun <joy.js.mun@gmail.com>
2022-06-11 19:14:30 +0900

    ADD: search result title list

--- src/routes/Search.tsx
+++ src/routes/Search.tsx
@@ -1,33 +1,50 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 import { useQuery } from "react-query";
-import { useSearchParams } from "react-router-dom";
+import { useMatch, useNavigate, useSearchParams } from "react-router-dom";
 import styled from "styled-components";
 import { getResults, IResults } from "../api";
+import { makeImagePath } from "../utils";
+
+
+const Wrapper = styled.div`
+background-color: black;
+
+`
+const Slider = styled.div`
+  background-color: tomato;
+  height: 100vh;
+  display: flex;
+  padding:60px;
+   position: relative;
+  top:130px;
+`
+
+const Row = styled(motion.div)`
+display: grid;
+grid-template-columns: repeat(6,1fr);
+grid-gap: 5px;
+position: absolute;
+width: 100%;
+`
 
 function Search(){
     const [searchParams] = useSearchParams();
     const keyword = searchParams.get("keyword");
     const {data,isLoading} = useQuery<IResults>(["search",keyword],() => getResults(keyword + ""));
-    
-    const Wrapper = styled.div`
-      position: absolute;
-      top:100px;
-    `
-
-    const Results = styled.div`
-      padding: 60px;
-    `
-    
-    return (
-      <Wrapper>
-            <Results>
-            {isLoading ? "...loading" : (
-            <>
-            {data?.results.map(item => <li>{item.title || item.original_title || item.original_name || item.name}</li>)}
-            </>
-        )}
-            </Results>
-      </Wrapper>
         
+    console.log(data)
+    return (
+   <Wrapper>
+     {isLoading ? "Loading..." : (
+     <>
+     <Slider>
+          <h4>Explore titles related to: {data?.results.map(item => <span>{item?.name || item?.original_title || item.title || item.original_name} | </span>)}</h4>
+          <Row></Row>
+       </Slider>
+     </>
+     )}
+   </Wrapper>     
     )
 }
 
