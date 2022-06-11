03c9d0e675f76ef2eb9db29337499fd80e87a46e
joyjsmun <joy.js.mun@gmail.com>
2022-06-11 17:39:36 +0900

    ADD: api issue solved

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 const BASE_URL = "https://api.themoviedb.org/3/"
 const API_KEY = process.env.REACT_APP_API_KEY
-//const API_KEY = "1658d974cfe4b0e4bc7eedbd02cc8660"
+
 
 export interface IMovie{
     backdrop_path: string,
